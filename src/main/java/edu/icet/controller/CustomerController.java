eded1a7165bb1e907125f4325b198798a11aeb28
sithija-jayasinghe <harshanajayasinghe113@gmail.com>
2025-12-22 08:52:34 +0530

    add Customer model and extend CustomerController with endpoints for customer data

--- src/main/java/edu/icet/controller/CustomerController.java
+++ src/main/java/edu/icet/controller/CustomerController.java
@@ -1,13 +1,31 @@
 package edu.icet.controller;
 
+import edu.icet.model.Customer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 public class CustomerController {
 
-    @GetMapping
-    public String getCustomerInfo() {
-        return "Customer Information";
+    ArrayList<Customer> customers = new ArrayList<>();
+
+    @GetMapping("/customer/name")
+    public String getCustomerName() {
+        return "Customer Name";
+    }
+
+    @GetMapping("/add-customer")
+    public Customer getCustomer() {
+        return new Customer("C001", "Sithija Jayasinghe", "harshanajayasinghe113@gmail.com", "Colombo", 5000.0);
+    }
+
+    @GetMapping("/customers/all")
+    public ArrayList<Customer> getAllCustomers() {
+        customers.add(new Customer("C001", "Sithija Jayasinghe", "harshanajayasinghe113@gmail.com", "Colombo", 5000.0));
+        customers.add(new Customer("C002", "John Doe", "johndoe@example", "New York", 3000.0));
+        customers.add(new Customer("C003", "Jane Smith", "janesmith@example", "Los Angeles", 4500.0));
+        return customers;
     }
 }
