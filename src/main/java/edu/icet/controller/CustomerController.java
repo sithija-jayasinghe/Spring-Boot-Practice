9091f4a72501f21f1cfbd66dbdbebb70c0d928c7
sithija-jayasinghe <harshanajayasinghe113@gmail.com>
2025-12-31 10:29:17 +0530

    add CORS support and endpoint to add customers in CustomerController

--- src/main/java/edu/icet/controller/CustomerController.java
+++ src/main/java/edu/icet/controller/CustomerController.java
@@ -1,12 +1,12 @@
 package edu.icet.controller;
 
 import edu.icet.model.Customer;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
 @RestController
+@CrossOrigin
 public class CustomerController {
 
     ArrayList<Customer> customers = new ArrayList<>();
@@ -23,9 +23,11 @@ public class CustomerController {
 
     @GetMapping("/customers/all")
     public ArrayList<Customer> getAllCustomers() {
-        customers.add(new Customer("C001", "Sithija Jayasinghe", "harshanajayasinghe113@gmail.com", "Colombo", 5000.0));
-        customers.add(new Customer("C002", "John Doe", "johndoe@example", "New York", 3000.0));
-        customers.add(new Customer("C003", "Jane Smith", "janesmith@example", "Los Angeles", 4500.0));
         return customers;
+    }
+
+    @PostMapping("/customers/add")
+    public void addCustomer(@RequestBody Customer customer) {
+        customers.add(customer);
     }
 }
